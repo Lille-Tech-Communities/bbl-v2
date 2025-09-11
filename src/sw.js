bf4d3f1497355f71b0d82ffc31e9dfb31827a58c
ANGELA RICCI <angela.ricci@worldline.com>
2025-09-11 14:49:07 +0200

    Added manifest, sw and icons for pwa

---
+++ src/sw.js
@@ -0,0 +1,38 @@
+const cacheName = 'bbl-01';
+
+const filesToCache = [
+  '/',
+  '/assets/css/Outfit-VariableFont_wght.ttf',
+  '/assets/css/vars.css'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+ e.respondWith(
+   fetch(e.request)
+   .catch(() => {
+     return caches.match(e.request).then(function(response) {
+       return response
+     })
+   })
+ );
+});
